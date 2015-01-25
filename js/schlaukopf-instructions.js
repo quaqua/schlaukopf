afe64728c4aeede86550242a3de1d16ee9a442d2
bine <contact@kneesandtoes.at>
2015-01-25 20:43:36 +0100

    up

--- js/schlaukopf-instructions.js
+++ js/schlaukopf-instructions.js
@@ -1,10 +1,50 @@
 SKMaths.addStep({
   title: '',
-  question: [ 209, '-', 111 ]
+  question: [ 209, '-', 109 ]
 });
 
 SKMaths.addStep({
   title: '',
-  question: [ 791, '-', 514 ]
+  question: [ 790, '-', 50 ]
 });
 
+SKMaths.addStep({
+  title: '',
+  question: [ 1000, '-', 25 ]
+});
+
+
+SKMaths.addStep({
+  title: '',
+  question: [ 88, '-', 11 ]
+});
+
+SKMaths.addStep({
+  title: '',
+  question: [ 110, '-', 70 ]
+});
+
+SKMaths.addStep({
+  title: '',
+  question: [ 560, '-', 90 ]
+});
+
+SKMaths.addStep({
+  title: '',
+  question: [ 601, '-', 500 ]
+});
+
+SKMaths.addStep({
+  title: '',
+  question: [ 420, '-', 50 ]
+});
+
+SKMaths.addStep({
+  title: '',
+  question: [ 900, '-', 450 ]
+});
+
+SKMaths.addStep({
+  title: '',
+  question: [ 77, '-', 25 ]
+});
